e49fc0a016f3252ca2f56f52061478c8ca7c6f56
sumori <hoangthiphuonguyen1602@gmail.com>
2024-06-11 17:28:48 +0700

    chore: Update ticketTypeController to improve code readability and consistency

--- controllers/ticketTypeController.js
+++ controllers/ticketTypeController.js
@@ -1,22 +1,31 @@
 // using handlerFactoryService to create a factory function for event controllers
 const handlerFactory = require("./handlerFactory");
 const catchAsync = require("./../utils/catchAsync");
-const TicketTypeModel  = require("../models/ticketTypeModel");
+const TicketTypeModel = require("../models/ticketTypeModel");
 const AppError = require("./../utils/appError");
 const TicketTypeService = require("./../services/ticketServices");
 
 exports.getTicketTypesByEventId = catchAsync(async (req, res) => {
-  const data = await TicketTypeService.getTicketTypesByEventId(req.params.id, req.query);
+  const data = await TicketTypeService.getTicketTypesByEventId(
+    req.params.id,
+    req.query
+  );
   res.status(200).json(data);
 });
 
 exports.createTicketType = catchAsync(async (req, res) => {
+  if (!tier) {
+     req.body.tier = undefined;
+  }
   const data = await TicketTypeService.createTicketType(req.body);
   res.status(201).json(data);
 });
 exports.getTicketType = handlerFactory.getOne(TicketTypeModel);
 exports.updateTicketType = catchAsync(async (req, res) => {
-  const data = await TicketTypeService.updateTicketType(req.params.id, req.body);
+  const data = await TicketTypeService.updateTicketType(
+    req.params.id,
+    req.body
+  );
   res.status(200).json(data);
 });
 exports.deleteTicketType = handlerFactory.deleteOne(TicketTypeModel);
@@ -25,7 +34,7 @@ exports.getAllTicketTypes = handlerFactory.getAll(TicketTypeModel);
 exports.updateTicketTypeSoldCount = catchAsync(async (req, res, next) => {
   const data = await TicketTypeService.updateTicketTypeSoldCount(req.params.id);
   next();
-})
+});
 
 exports.getTicketTypeByTierId = catchAsync(async (req, res) => {
   const data = await TicketTypeService.getTicketTypeByTierId(req.params.id);
